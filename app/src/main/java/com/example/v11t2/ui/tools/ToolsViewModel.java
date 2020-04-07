1ceaa4057859a28cd84b7922282c30cdade7823b
markuslat <markus.latvakoski@student.lut.fi>
2020-04-07 19:30:43 +0300

    Initial commit

---
+++ app/src/main/java/com/example/v11t2/ui/tools/ToolsViewModel.java
@@ -0,0 +1,19 @@
+package com.example.v11t2.ui.tools;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class ToolsViewModel extends ViewModel {
+
+    private MutableLiveData<String> mText;
+
+    public ToolsViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("This is settings fragment");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}
